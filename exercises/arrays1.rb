cb17572ac07347a933c383d0554b274ad33b8e18
Alexandra <belliraxia@gmail.com>
2022-03-07 15:53:41 -0500

    adds exercise files dir

---
+++ exercises/arrays1.rb
@@ -0,0 +1,75 @@
+# --------------------------------------------
+
+# 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+# 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+# 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+# 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+# 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+# 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+# 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+
+# 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+# 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+# 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+# --------------------------------------------
+
+# words = ["bellicose", "ataraxia", "rivulet"]
+# words << "word"
+# words << "anotherword"
+# p words
+
+# arr = ["a", "b", "c", "d"]
+# arr[1] = 2
+# p arr
+
+# arr = [1, 2, 3, 4, 5]
+# index = 0
+# while index < arr.length
+#   puts arr[index]
+#   index += 1
+# end
+
+# arr = [1]
+# arr << 2
+# arr << 3
+# arr << 4
+# p arr
+
+# strings = ["hello there", "not today", "ummm ok"]
+# strings[2].upcase!
+# p strings
+
+# names = ["regina", "basileus", "sandalphon"]
+# index = 0
+# while index < names.length
+#   p names[index]
+#   index += 1
+# end
+
+# strings = ["hello there", "goodbye fella"]
+# strings << "well ok then"
+# p strings
+
+# nums = [1, 2, 3, 4, 5]
+# nums[0] *= 10
+# p nums
+
+# nums = [5, 10]
+# index = 0
+# while index < nums.length
+#   puts nums[index]
+#   index += 1
+# end
+
+# countries = ["mexico", "canada", "united states"]
+# countries << "argentina"
+# p countries
